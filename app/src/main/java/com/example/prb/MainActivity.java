605a6f17d1e122a47ed0d69945d0fc8b02da9105
BorisMartinMedJorDSII <67562068+BorisMartinMedJorDSII@users.noreply.github.com>
2020-11-26 19:23:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/prb/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.prb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void MRD(View view) {
+        Intent putos = new Intent(MainActivity.this, lol.class);
+        startActivity(putos);
+    }
+}
---
+++ app/src/main/java/com/example/prb/lol.java
@@ -0,0 +1,14 @@
+package com.example.prb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lol);
+    }
+}
